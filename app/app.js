8866f9d04c196620fe05322fecdaf17273746ffd
dphengsiaroun <dphengsiaroun@outlook.fr>
2016-10-05 16:46:07 +0200

    angular rest

--- app/app.js
+++ app/app.js
@@ -7,7 +7,40 @@
 		$('.flexslider').flexslider();
 	});
 
-	var app = angular.module('mainApp', []);
+	var app = angular.module('mainApp', ['ngRoute']);
+
+	app.config(['$routeProvider', function($routeProvider) {
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'tmpl/home.html'
+			})
+			.when('/signin', {
+				templateUrl: 'tmpl/signin.html'
+			})
+			.when('/signup', {
+				templateUrl: 'tmpl/signup.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
+
+	app.run(['$rootScope', '$http', function($rootScope, $http) {
+		$rootScope.isConnected = false;
+		$rootScope.signinData = {};
+		$rootScope.signin = function() {
+			console.log('sign in');
+			$http({
+				url: 'ws/signin.php',
+				method: 'POST',
+				data: $rootScope.signinData,
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+			}).then(function(response) {
+				console.log('response', response);
+			});
+		};
+	}]);
 
 	app.directive('jlgHeader', function() {
 		console.log('Hello');
@@ -23,5 +56,14 @@
 			templateUrl: 'tmpl/jlg-footer.html'
 		};
 	});
+
+	app.directive('jlgBanner', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/jlg-banner.html'
+		};
+	});
+
+
 
 })();
