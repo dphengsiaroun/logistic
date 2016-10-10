5ac92637c10411a94da3b42a62c6d1235c0ada14
dphengsiaroun <dphengsiaroun@outlook.fr>
2016-10-10 15:44:07 +0200

    mise en page + travail sur fichier signin.php

--- app/ws/signin.php
+++ app/ws/signin.php
@@ -2,4 +2,24 @@
 
 	echo '{"result":"ok"}';
 
+	$host = 'localhost';
+	$user = 'root';
+	$mdp = 'root';
+	$bdd = 'logistic';
+
+	$connex = new mysqli($host, $user, $mdp, $bdd);
+
+	$pseudo = $_POST['pseudo'];
+	$mdp = $_POST['mdp'];
+
+	$req = "SELECT * FROM membre WHERE pseudo='$pseudo' AND mdp='$mdp'";
+	$lancereq = $connex->query($req);
+  
+   //Récupérer tous les détails de Angular HTTP Request  
+    $postdata = file_get_contents ( "php: // input" ) ; 
+    $request = json_decode ($postdata) ; 
+    @ $login = $request->login ; 
+    @ $passe = $request->password ; 
+    echo $login ;
+
 ?>
