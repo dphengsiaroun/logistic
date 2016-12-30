7b76d89f6a8eb283f157b582d9a79d5d3cf443a2
Phengsiaroun <dany.phengsiaroun@estiam.com>
2016-12-30 17:02:38 +0100

    add: gulp

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+var del = require('del');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+//webpackConfig.setupProd();
+var eslint = require('gulp-eslint');
+var through = require('through2');
+var open = require('open');
+
+gulp.task('default', ['rebuild']);
+
+var path = {
+	base: 'app',
+	dist: 'dist',
+	html: ['app/index.html'],
+	webpack: ['app/app.js'],
+	resources: ['app/resources/**/*', 'app/wpk/**/*']
+};
+
+
+// Delete the dist directory
+gulp.task('clean', function() {
+	return del(path.dist);
+});
+
+gulp.task('resources', function() {
+	return gulp.src(path.resources, {base: path.base})
+		.pipe(gulp.dest(path.dist));
+});
+
+gulp.task('html', function() {
+	return gulp.src(path.html)
+		.pipe(gulp.dest(path.dist));
+});
+
+gulp.task('webpack', function(callback) {
+    webpack(webpackConfig, function(err, stats) {
+        if (err) {
+			throw new gutil.PluginError('webpack', err);
+		}
+        callback();
+    });
+});
+
+gulp.task('build', function() {
+	console.log('gulp build');
+	runSequence('webpack', ['resources', 'html']);
+});
+
+gulp.task('rebuild', function() {
+	runSequence('clean', 'build');
+});
+
+gulp.task('lint', function() {
+	return gulp.src(['**/*.js'])
+	.pipe(eslint())
+	.pipe(eslint.formatEach())
+	.pipe(eslint.failAfterError());
+});
