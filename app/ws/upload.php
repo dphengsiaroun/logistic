f535c4c20b2fbeae9b4838d3228c2cb6a533fc2d
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-17 09:52:18 +0100

    add: upload image

--- app/ws/upload.php
+++ app/ws/upload.php
@@ -6,49 +6,37 @@ require_once(BASE_DIR . "/include/account.inc.php");
 error_reporting(E_ALL | E_STRICT);
 require(BASE_DIR . '/include/lib/UploadHandler.php');
 
-class MyUploadHandler extends UploadHandler
-{
+class MyUploadHandler extends UploadHandler {
 	protected function get_user_id() {
 		$account = Account::getConnected();
 		return $account->getPictureDir();
 	}
 }
 
-class MySessionUploadHandler extends UploadHandler
-{
+class MySessionUploadHandler extends UploadHandler {
 	protected function get_user_id() {
 		return 'session_' . session_id();
 	}
 }
 
+$options = array(
+	'upload_dir' => UPLOAD_DIR,
+	'upload_url' => UPLOAD_URL,
+	'script_url' => 'ws/upload.php',
+	'user_dirs' => true,
+	'mkdir_mode' => 0705,
+);
+
 try {
 	if (Account::isConnected()) {
 		$account = Account::getConnected();
-		
-		
-		$options = array(
-			'upload_dir' => UPLOAD_DIR,
-			'upload_url' => UPLOAD_URL,
-			'script_url' => 'ws/upload.php',
-			'user_dirs' => true,
-			'mkdir_mode' => 0705,
-			'max_file_size' => $account->getRemainingMaxFileSize()
-		);
-		
+		$options['max_file_size'] = $account->getRemainingMaxFileSize();
 		$upload_handler = new MyUploadHandler($options);
 		debug('output', $upload_handler);
 		$account->reportLoadedPicture();
 		debug('done');
 	} else {
-		$options = array(
-			'upload_dir' => UPLOAD_DIR,
-			'upload_url' => UPLOAD_URL,
-			'script_url' => 'ws/upload.php',
-			'user_dirs' => true,
-			'mkdir_mode' => 0705,
-			'max_file_size' => MAX_PICTURE_SIZE_PER_ANONYMOUS_SESSION
-		);
-		
+		$options['max_file_size'] = MAX_PICTURE_SIZE_PER_ANONYMOUS_SESSION;
 		$upload_handler = new MySessionUploadHandler($options);
 		debug('output', $upload_handler);
 		debug('done');
@@ -57,11 +45,13 @@ try {
 	
 	
 } catch (Exception $e) {
-	$result = array('files' => array(
-		array(
-		'error' => $e->getMessage()
+	$result = array(
+		'files' => array(
+			array(
+				'error' => $e->getMessage()
+			)
 		)
-		));
+	);
 	echo json_encode($result);
 }
 
