05cc492d0fac097c13915aaab226d5ac75f5718f
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-17 17:17:05 +0100

    add: lg-hour

--- app/lg-calendar/lg-hour.js
+++ app/lg-calendar/lg-hour.js
@@ -1,10 +1,11 @@
 'use strict';
 
-require('./lg-hour.css');
+require('./lg-hour.scss');
 
 var app = angular.module('lg-calendar');
 
 var lgHourUrl = require('./tmpl/lg-hour.html');
+var id = 0;
 
 app.component('lgHour', {
 	require: {
@@ -13,14 +14,35 @@ app.component('lgHour', {
 	templateUrl: lgHourUrl,
 	controller: function LgMonthCtrl($scope, $element, $locale, $compile) {
 		var self = this;
+		id++;
+		this.id = id;
 		// console.log('lgMonth ctrl', this, arguments);
 		this.$onInit = function() {
 			console.log('lgHour ctrl $onInit', this);
+			this.refreshStyle();
 		};
 
 		this.$onChanges = function(map) {
 			if (map.selectedHours !== undefined) {
 				this.refresh();
+			}
+		};
+
+		$scope.$watchGroup(['$ctrl.radius', '$ctrl.x', '$ctrl.y'], function() {
+			self.refreshStyle();
+		});
+
+		this.radius = 110;
+		this.x = 107;
+		this.y = 109;
+
+		this.refreshStyle = function() {
+			this.style = '';
+
+			for (var i = 1; i <= 12; i++) {
+				var top = this.y - this.radius*Math.cos(i*6.28/12);
+				var left = this.x + this.radius*Math.sin(i*6.28/12);
+				this.style += '.id-' + this.id + '.hour-' + i + ' { top: ' + top + 'px; left: ' + left + 'px;}\n';
 			}
 		};
 
@@ -44,7 +66,7 @@ app.component('lgHour', {
 				hour = 12;
 			}
 			var ampm = (this.selectedHours > 12 || this.selectedHours === 0) ? 'pm' : 'am';
-			var myClass = 'h' + hour + ' ' + ampm;
+			var myClass = 'hour-' + hour + ' ' + ampm;
 			var newSelectedElt = angular.element($element[0].getElementsByClassName(myClass));
 			newSelectedElt.addClass('selected');
 		};
