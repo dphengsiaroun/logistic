ab704c7a75a26bee2bf6f849c6233b6e63ee594c
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-18 10:51:20 +0100

    add: stats.sh

--- stats.sh
+++ stats.sh
@@ -4,7 +4,14 @@ set -eau
 set +e
 (
     set -eau
-    echo 'coucou'
+    FILE_LIST=`find . -type f | grep -v 'node_modules' | grep -v 'dist' | grep -v '/app/ws/vendor/' |\
+        grep -v '/app/wpk/' | grep -v './.git' | grep -v './app/favicon' |\
+        grep -v '/app/img/' | grep -v '.zip' | grep -v '/app/files'`
+    #echo "$FILE_LIST"
+    FILE_COUNT=`echo "$FILE_LIST" | wc -l`
+    echo "FILE_COUNT=$FILE_COUNT"
+    STAT=`wc $FILE_LIST`
+    echo "STAT=$STAT"
 )
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
