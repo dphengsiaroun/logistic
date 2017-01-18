d7452671539bc5d31402e7b670b0a73bae64db6c
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-18 10:59:24 +0100

    add: stats.sh

--- stats.sh
+++ stats.sh
@@ -6,7 +6,7 @@ set +e
     set -eau
     FILE_LIST=`find . -type f | grep -v 'node_modules' | grep -v 'dist' | grep -v '/app/ws/vendor/' |\
         grep -v '/app/wpk/' | grep -v './.git' | grep -v './app/favicon' |\
-        grep -v '/app/img/' | grep -v '.zip' | grep -v '/app/files'`
+        grep -v '/app/img/' | grep -v '.zip$' | grep -v '/app/files' | grep -v '.log$' | grep -v '.lock$'`
     #echo "$FILE_LIST"
     FILE_COUNT=`echo "$FILE_LIST" | wc -l`
     echo "FILE_COUNT=$FILE_COUNT"
