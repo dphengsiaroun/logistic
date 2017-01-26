9559174557a00d58dcf0517edf6f8e4b39cc7895
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-26 17:36:29 +0100

    add: lg-datetime 4

--- app/lg-datetime/lg-dt-hour.js
+++ app/lg-datetime/lg-dt-hour.js
@@ -10,7 +10,16 @@ app.component('lgDtHour', {
 	},
 	templateUrl: lgDtHourUrl,
 	controller: function LgDtHourCtrl($scope, $element, $locale, $compile) {
-
+		var ctrl = this;
+		ctrl.update = function(hour) {
+			console.log('update', arguments);
+			var elt = $element.find('line');
+			elt.css('left', (44-(hour*14.6)) + '%');
+			var allHourElt = $element.find('line span');
+			allHourElt.removeClass('selected');
+			var hourElt = angular.element($element[0].querySelector('line .h' + hour));
+			hourElt.addClass('selected');
+		};
 	},
 	bindings: {
 		action: '<',
