9f2b2e02d823ee7467434784c4e3c1b75dfbc949
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-01-30 15:52:16 +0100

    add: lg-datetime scrolling

--- app/lg-datetime/lg-dt-hour.js
+++ app/lg-datetime/lg-dt-hour.js
@@ -9,19 +9,53 @@ app.component('lgDtHour', {
 		lgDatetime: '^^lgDatetime'
 	},
 	templateUrl: lgDtHourUrl,
-	controller: function LgDtHourCtrl($scope, $element, $locale, $compile) {
+	controller: function LgDtHourCtrl($scope, $element, $window) {
 		var ctrl = this;
+		var hourElt;
+		var lineElt;
+		var isUpdating = false;
+		var width = screen.width;
 		ctrl.update = function(hour) {
 			console.log('LgDtHourCtrl update', arguments);
-			console.log('$element', $element.html());
-			var elt = $element.find('line');
-			elt.css('left', (44-(hour*14.6)) + '%');
+			isUpdating = true;
+			console.log('width', width);
+			hourElt[0].scrollLeft = (2.5 + hour * 1.5) * width/10;
 			this.action.apply(null, [hour]);
 		};
 
 		ctrl.$onInit = function() {
 			console.log('LgDtHourCtrl $onInit', arguments);
+			hourElt = $element.find('hours');
+			lineElt = hourElt.find('line');
+			hourElt[0].onscroll = onScroll;
 			ctrl.update(ctrl.selectedHours);
+		};
+
+		ctrl.swipe = function() {
+			console.log('swipe', arguments);
+			var pos = hourElt[0].scrollLeft;
+			console.log('pos', pos);
+			var hour = Math.round(((pos-100) * 6.8) / width);
+			console.log('hour', hour);
+			ctrl.update(hour);
+		};
+
+		var isScrolling = false;
+
+		var onScroll = function() {
+			if (isUpdating === true) {
+				isUpdating = false;
+				return;
+			}
+			console.log('onScroll', arguments);
+			if (isScrolling === false) {
+				isScrolling = true;
+				setTimeout(function() {
+					ctrl.swipe();
+					$scope.$apply();
+					isScrolling = false;
+				}, 100);
+			}
 		};
 
 	},
