e7aab9ab22a37b490e0db594d34f43243813f727
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-02-01 17:35:22 +0100

    add: lg-loader dimension

--- app/lg-misc/lg-misc.js
+++ app/lg-misc/lg-misc.js
@@ -5,14 +5,14 @@ module.exports = 'lg-misc';
 var app = angular.module(module.exports, []);
 
 app.service('lgMisc', ['$injector', function LgMisc($injector) {
-	this.isWebService = function(url) {
+	this.isWebService = function (url) {
 		return url.match(/ws\/.*\.php/);
 	};
 }]);
 
 app.service('lgSequence', function LgSequence() {
 	this.current = 0;
-	this.next = function() {
+	this.next = function () {
 		this.current++;
 		return this.current;
 	};
@@ -21,17 +21,17 @@ app.service('lgSequence', function LgSequence() {
 app.service('lgScroll', ['$injector', function LgScroll($injector) {
 	var $window = $injector.get('$window');
 	this.lastSaved = 0;
-	this.save = function() {
+	this.save = function () {
 		this.lastSaved = $window.scrollY;
 	};
-	this.restore = function() {
+	this.restore = function () {
 		$window.scrollTo(0, this.lastSaved);
 	};
 }]);
 
 app.service('lgFormat', function LgFormat($filter) {
 	'ngInject';
-	this.formatDuration = function(duration) {
+	this.formatDuration = function (duration) {
 		var result = '';
 		var minuteFormat = 'mm';
 		var hourFormat = ' et \'H\'h\'';
@@ -41,12 +41,25 @@ app.service('lgFormat', function LgFormat($filter) {
 				hourFormat = '';
 			}
 		}
-		if (duration < 24*60*60) {
-			result = $filter('date')(duration*1000, 'H\'h\'' + minuteFormat, 'UTC');
+		if (duration < 24 * 60 * 60) {
+			result = $filter('date')(duration * 1000, 'H\'h\'' + minuteFormat, 'UTC');
 		} else {
 
-			result = $filter('date')((duration - 24*60*60)*1000, 'd\'j' + hourFormat + minuteFormat, 'UTC');
+			result = $filter('date')((duration - 24 * 60 * 60) * 1000, 'd\'j' + hourFormat + minuteFormat, 'UTC');
 		}
 		return result;
+	};
+});
+
+app.filter('distance', function() {
+	'ngInject';
+	return function(number) {
+		var integer = Math.floor(number);
+		var decimal = Math.round((number - integer) * 100);
+		var decimalStr = '0' + decimal;
+		if (decimal > 10) {
+			decimalStr = '' + decimal;
+		} 
+		return integer + 'm' + decimalStr;
 	};
 });
