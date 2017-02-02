6b72069360238fb38a40acb71cbe9e66e4c91edc
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-02-02 11:04:33 +0100

    add: lgDimension

--- app/lg-dimension/lg-dimension.js
+++ app/lg-dimension/lg-dimension.js
@@ -13,10 +13,34 @@ app.component('lgDimension', {
 		ngModel: 'ngModel',
 	},
 	templateUrl: lgDimensionUrl,
-	controller: function LgDimensionCtrl() {
+	controller: function LgDimensionCtrl(lgScroll) {
 		'ngInject';
 		console.log('LgDimensionCtrl');
 		var ctrl = this;
+
+		ctrl.start = function() {
+			console.log('start', arguments);
+			ctrl.state = 'editState';
+			lgScroll.save();
+		};
+
+		ctrl.stop = function() {
+			ctrl.state = 'outsideState';
+			lgScroll.restore();
+			ctrl.months = [];
+		};
+
+		ctrl.update = function(dimension) {
+			ctrl.dimension = dimension;
+			ctrl.ngModel.$setViewValue(dimension);
+			ctrl.ngModel.$render();
+			ctrl.ngModel.$setTouched();
+		};
+
+		ctrl.cancel = function() {
+			ctrl.update(undefined);
+			ctrl.stop();
+		};
 	},
 	bindings: {
 		
