a2a21410850b3bd89a9a39694afbe6e2655f1a33
Phengsiaroun <dany.phengsiaroun@estiam.com>
2017-02-08 11:28:47 +0100

    add: svg ngTemplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 
 const webpack = require('webpack');
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: {
-        'app.prod': './app/app.js',
+		'app.prod': './app/app.js',
 		'install.prod': './app/install/install.js'
-    },
+	},
 	output: {
 		path: './app/wpk',
 		filename: '[name].js'
 	},
 	plugins: [
 		new ExtractTextPlugin('[name].css')
-    ],
+	],
 	module: {
 		loaders: [
 			{test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate!babel'},
@@ -23,23 +24,23 @@ module.exports = {
 			{test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
 			{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
 			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-			{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+			{test: /fontawesome-webfont\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
 			// managing angular templates into javascript file.
-			{test: /\.html$/, loader: 'ngtemplate?relativeTo=app!html?attrs=false'}
-
+			{test: /\.html$/, loader: 'ngtemplate?relativeTo=app!html?attrs=img-svg:src&root=' + path.resolve('./app')},
+			{test: /img\/.+\.svg$/, loader: 'ngtemplate?relativeTo=app!html?attrs=false'}
 		]
-    },
+	},
 	devtool: 'source-map',
 	setupProd: function() {
 		// console.log('setupProd', this);
 		this.plugins.push(new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            }
-        }));
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			}
+		}));
 	},
 	resolve: {
 		extensions: ['', '.js'],
@@ -48,7 +49,7 @@ module.exports = {
 			'load-image-meta': 'blueimp-load-image/js/load-image-meta.js',
 			'load-image-exif': 'blueimp-load-image/js/load-image-exif.js',
 			'canvas-to-blob': 'blueimp-canvas-to-blob/js/canvas-to-blob.js',
-			'jquery-ui/ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js'
+			'jquery-ui/ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js',
 		}
 	}
 };
