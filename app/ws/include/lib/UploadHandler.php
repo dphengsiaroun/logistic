a40ce2290bc2439327b8b1da4dc9a080030580d7
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-03-11 17:47:21 +0100

    add: upload resize thumbnail img

--- app/ws/include/lib/UploadHandler.php
+++ app/ws/include/lib/UploadHandler.php
@@ -155,8 +155,8 @@ class UploadHandler
                     // Uncomment the following to force the max
                     // dimensions and e.g. create square thumbnails:
                     //'crop' => true,
-                    'max_width' => 80,
-                    'max_height' => 80
+                    'max_width' => 140,
+                    'max_height' => 140
                 )
             ),
             'print_response' => true
