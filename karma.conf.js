c707b7629724ec9349efaa998cd9559f6531d4d0
Maïté THOMIAS <maite.thomias@gmail.com>
2017-07-07 13:39:42 +0200

    add: conf karma

--- karma.conf.js
+++ karma.conf.js
@@ -6,10 +6,6 @@ const webpackKarmaConfig = require('./webpack.karma.config.js');
 module.exports = function(config) {
 	config.set({
 
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: '',
-
-
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['jasmine'],
@@ -20,11 +16,6 @@ module.exports = function(config) {
 			'test/unit/index.js',
 		],
 
-
-		// list of files to exclude
-		exclude: [],
-
-
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
@@ -32,6 +23,9 @@ module.exports = function(config) {
 		},
 
 		webpack: webpackKarmaConfig,
+		webpackMiddleware: {
+			noInfo: 'errors-only'
+		},
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
@@ -46,22 +40,10 @@ module.exports = function(config) {
 			}]
 		},
 
-
-		// web server port
-		port: 9876,
-
-
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
-
-
 		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
-
-
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN 
+		// || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_WARN,
 
 
 		// start these browsers
@@ -73,8 +55,9 @@ module.exports = function(config) {
 		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: true,
 
-		// Concurrency level
-		// how many browser should be started simultaneous
-		concurrency: Infinity
+		client: {
+			// log console output in our test console (false: no log)
+			captureConsole: false
+		},
 	});
 };
