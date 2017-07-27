5e591330cb95a3c6a3d6d1c5da6f0364ceebc981
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-07-27 14:47:20 +0200

    add: lg-erasable

--- app/modules/form/lg-filter/lg-filter.js
+++ app/modules/form/lg-filter/lg-filter.js
@@ -17,16 +17,17 @@ app.component('lgFilter', {
 		ctrl.isEditing = false;
 
 		ctrl.edit = () => {
-			console.log('edit');
+			console.log('ctrl.edit');
 			ctrl.isEditing = true;
 		};
 
 		ctrl.stop = () => {
+			console.log('ctrl.stop');
 			ctrl.isEditing = false;
 		};
 
 		ctrl.submit = () => {
-			
+
 		};
 
 	},
--- app/modules/technic/lg-erasable/lg-erasable.js
+++ app/modules/technic/lg-erasable/lg-erasable.js
@@ -5,12 +5,27 @@ const app = angular.module(module.exports, []);
 app.directive('lgErasable', () => {
 	'ngInject';
 	return {
-        restrict: 'A',
-		controller: function LgErasableCtrl($element) {
+		restrict: 'A',
+		controller: function LgErasableCtrl($scope, $element, $compile) {
 			'ngInject';
-            console.log('LgErasableCtrl', arguments);
+			console.log('LgErasableCtrl', arguments);
+			const ctrl = this;
 
-            $element.after('<i class="fa fa-times-circle erasable" aria-hidden="true"></i>');
+			$scope.lgErasableCtrl = ctrl;
+
+			const elt = angular.element(`<div 
+				class="erase" 
+				ng-click="lgErasableCtrl.erase()">
+					<i class="fa fa-times-circle erasable" aria-hidden="true"></i>
+				</div>`);
+			$element.after(elt);
+			$compile(elt)($scope);
+			
+			ctrl.erase = () => {
+				console.log('ctrl.erase');
+			};
+
+			
 		}
 	};
 });
