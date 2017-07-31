831a93bf664d94ecc542be9ebe2526b2a8948957
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-07-31 16:45:50 +0200

    add: e2e-orderby

--- test/e2e/cfg/orderby.js
+++ test/e2e/cfg/orderby.js
@@ -1,6 +1,7 @@
-const {config} = require('../../../protractor.conf.js');
+const { config } = require('../../../protractor.conf.js');
 config.specs = [
 	'../e2e-install.js',
+	'../e2e-geoloc-stub.js',
 	'../e2e-orderby.js',
 ];
 exports.config = config;
