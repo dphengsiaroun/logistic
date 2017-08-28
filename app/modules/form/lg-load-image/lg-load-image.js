35afee2b281ad189fb9f895d04b0b8672e9df73e
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-08-28 12:04:08 +0200

    add: delete lg-load-image

--- app/modules/form/lg-load-image/lg-load-image.js
+++ app/modules/form/lg-load-image/lg-load-image.js
@@ -11,19 +11,25 @@ import lgLoadImageHtml from './tmpl/lg-load-image.html';
 app.component('lgLoadImage', {
 	template: lgLoadImageHtml,
 	require: {ngModel: 'ngModel'},
+	bindings: {
+		formData: '='
+	},
 	controller: function LgLoadImageCtrl($element, $scope, lgImageLoader) {
 		'ngInject';
 		console.log('LgLoadImageCtrl', arguments);
 		const ctrl = this;
+		
 		ctrl.active = false;
 		ctrl.progress = 0;
 		ctrl.$onInit = function() {
+			const imageLoader = lgImageLoader.newInstance(ctrl);
+			imageLoader.init();
 			const inputElt = $element.find('input');
 			console.log('inputElt', inputElt);
 			inputElt.on('change', function() {
 				console.log('change', arguments, this);
 				console.log('inputElt', inputElt);
-				lgImageLoader.send(inputElt, ctrl);
+				imageLoader.send(inputElt);
 			});
 		};
 	}
--- app/modules/form/lg-load-image/lgImageLoader.service.js
+++ app/modules/form/lg-load-image/lgImageLoader.service.js
@@ -1,71 +1,100 @@
 const url = './ws/upload.php';
 
+
+
 export function LgImageLoader($http, $rootScope, lgPicture) {
 	'ngInject';
-	this.send = function(inputElt, ctrl) {
-		console.log('about to send');
-		const name = inputElt[0].files[0].name;
-        console.log('name', name);
-        
-        ctrl.file = undefined;
 
-		const formData = new FormData();
-		const file = inputElt[0].files[0];
+	function ImageLoader(ctrl) {
+		ctrl.file = undefined;
 
-		if (!file.type.match('image.*')) {
-			console.error('file is not an image');
-			return;
+		function setFile(file) {
+			ctrl.file = file;
+			ctrl.file.$destroy = function() {
+				return $http({
+					url: ctrl.file.deleteUrl + '&suffix=' + ctrl.formData.suffix,
+					method: ctrl.file.deleteType
+				}).then(function(response) {
+					console.log('response', response);
+					ctrl.file = undefined;
+				}).catch(function(error) {
+					console.error('error', error);
+					ctrl.file = undefined;
+				});
+			};
 		}
 
-		// Add the file to the request.
-		formData.append('files[]', file, file.name);
-
-		const xhr = new XMLHttpRequest();
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-				// File(s) uploaded.
-				ctrl.active = false;
-                console.log('Upload successful', xhr.response);
-                const json = JSON.parse(xhr.response);
-                console.log('json', json);
-				ctrl.file = json.files[0];
-
-				ctrl.file.$destroy = function() {
-					return $http({
-						url: ctrl.file.deleteUrl + '&suffix=' + formData.suffix,
-						method: ctrl.file.deleteType
-					}).then(function(response) {
-						console.log('response', response);
-					}).catch(function(error) {
-						console.error('error', error);
-					});
-				};
-
-			} else {
-                ctrl.file = undefined;
-				console.error('Upload not successful');
-            }
-            $rootScope.$apply();
-		};
-		console.log('xhr', xhr);
-		xhr.upload.addEventListener('progress', function(evt) {
-			if (evt.lengthComputable) {
-				let percentComplete = evt.loaded / evt.total;
-				percentComplete = parseInt(percentComplete * 100);
-				console.log(percentComplete);
-
-				if (percentComplete === 100) {
-					console.log('complete');
+		this.init = function() {
+			$http.get(url + '?suffix=' + ctrl.formData.suffix).then(function(response) {
+				console.log('$http get return', response);
+				if (response.data.files && response.data.files.length > 0) {
+					setFile(response.data.files[0]);
 				}
-				ctrl.progress = percentComplete;
-				$rootScope.$apply();
+			}).catch(function(error) {
+				console.log('$http get error', error);
+			});
+		};
+
+		this.send = function(inputElt) {
+			console.log('about to send');
+			const name = inputElt[0].files[0].name;
+			console.log('name', name);
+
+			ctrl.file = undefined;
+
+			const formData = new FormData();
+			const file = inputElt[0].files[0];
+
+			if (!file.type.match('image.*')) {
+				console.error('file is not an image');
+				return;
 			}
-		}, false);
-		xhr.open('POST', url, true);
 
-		xhr.send(formData);
-		ctrl.active = true;
-		$rootScope.$apply();
+			// Add the file to the request.
+			formData.append('files[]', file, file.name);
 
+			const xhr = new XMLHttpRequest();
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+					// File(s) uploaded.
+					ctrl.active = false;
+					console.log('Upload successful', xhr.response);
+					const json = JSON.parse(xhr.response);
+					console.log('json', json);
+					setFile(json.files[0]);
+
+				} else {
+					ctrl.file = undefined;
+					console.error('Upload not successful');
+				}
+				$rootScope.$apply();
+			};
+			console.log('xhr', xhr);
+			xhr.upload.addEventListener('progress', function(evt) {
+				if (evt.lengthComputable) {
+					let percentComplete = evt.loaded / evt.total;
+					percentComplete = parseInt(percentComplete * 100);
+					console.log(percentComplete);
+
+					if (percentComplete === 100) {
+						console.log('complete');
+					}
+					ctrl.progress = percentComplete;
+					$rootScope.$apply();
+				}
+			}, false);
+			xhr.open('POST', url, true);
+
+			xhr.send(formData);
+			ctrl.active = true;
+			$rootScope.$apply();
+
+		};
+	}
+
+	this.newInstance = function(ctrl) {
+		return new ImageLoader(ctrl);
 	};
+
+
 }
