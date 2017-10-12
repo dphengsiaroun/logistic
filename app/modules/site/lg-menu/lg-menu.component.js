64457f883f4883de5255f2516195f25a54d02708
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-10-12 15:32:37 +0200

    add: lg-menu

--- app/modules/site/lg-menu/lg-menu.component.js
+++ app/modules/site/lg-menu/lg-menu.component.js
@@ -43,7 +43,7 @@ export const lgMenu = {
 				ctrl.myProposals = proposals;
 				console.log('ctrl.myProposals', ctrl.myProposals);
 			}).catch(function(error) {
-				console.error('error', error);
+				console.log('error', error);
 			});
 		};
 		ctrl.refreshNotifications();
