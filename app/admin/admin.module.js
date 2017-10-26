d85952aecbffa44caac8a19ee892e49961bd66ac
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-10-26 16:11:57 +0200

    refactor: admin module

--- app/admin/admin.module.js
+++ app/admin/admin.module.js
@@ -3,26 +3,30 @@ module.exports = 'admin';
 
 import 'angular-sanitize';
 
+import adminCarrier from './admin-carrier/admin-carrier.module.js';
+import adminConnection from './admin-connection/admin-connection.module.js';
+import adminLoader from './admin-loader/admin-loader.module.js';
+import adminProposal from './admin-proposal/admin-proposal.module.js';
+import adminUser from './admin-user/admin-user.module.js';
+
 import lgDebug from '../modules/technic/lg-debug/lg-debug.service.js';
 import lgMisc from '../modules/technic/lg-misc/lg-misc.module.js';
 import lgConfig from '../modules/business/lg-config/lg-config.module.js';
+
 import { adminPrompt } from './widget/admin-prompt.component.js';
 import { adminConfirm } from './widget/admin-confirm.component.js';
-import adminConnection from './admin-connection/admin-connection.module.js';
-import adminUser from './admin-user/admin-user.module.js';
-import adminCarrier from './admin-carrier/admin-carrier.module.js';
-import adminLoader from './admin-loader/admin-loader.module.js';
-import adminProposal from './admin-proposal/admin-proposal.module.js';
+
 import * as lib from './admin-route.js';
 
 angular.module(module.exports, [
 	'ui.router', 
 	'ngSanitize',
+	adminCarrier,	
 	adminConnection,
-	adminUser,
-	adminCarrier, 
 	adminLoader, 
-	adminProposal, 
+	adminProposal,
+	adminUser,	
+
 	lgConfig, 
 	lgMisc, 
 	lgDebug
