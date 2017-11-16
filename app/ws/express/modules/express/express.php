520c0b8eb55013ad5f4b2fb8ab8acf6d7c1f9724
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-11-16 15:45:55 +0100

    add: express php

--- app/ws/express/modules/express/express.php
+++ app/ws/express/modules/express/express.php
@@ -12,17 +12,17 @@ class Express {
         $this->res = new Response();
         $this->middlewares = array();
         $this->i = 0;
+    }
+
+    public function use($middleware) {
+        array_push($this->middlewares, $middleware);
+    }
+
+    public function run() {
         $this->use(function($req, $res, $next) {
             $res->status(404);
             $res->send('Not found...');
         });
-    }
-
-    public function use($middleware) {
-        array_unshift($this->middlewares, $middleware);
-    }
-
-    public function run() {
         $this->next();
         $this->res->deploy();
     }
--- app/ws/express/server.php
+++ app/ws/express/server.php
@@ -7,7 +7,15 @@ $app = new Express();
 $app->use(function($req, $res, $next) {
     global $console;
     $console->log('$req->url', $req->url);
-    $res->send("Coucou: $req->url");
+    $res->send("Coucoux: $req->url");
+    $next();
 });
+
+$app->use(function($req, $res, $next) {
+    global $console;
+    $console->log('$req->url', $req->url);
+    $res->send("Coucou2: $req->url");
+});
+
 
 $app->run();
