36a18c05f6e6761eeeeff5df6c655f476426a168
dphengsiaroun <dphengsiaroun@outlook.fr>
2017-11-16 16:18:58 +0100

    add: url

--- app/ws/express/modules/express/express.php
+++ app/ws/express/modules/express/express.php
@@ -14,8 +14,36 @@ class Express {
         $this->i = 0;
     }
 
-    public function use($middleware) {
-        array_push($this->middlewares, $middleware);
+    public function use() {
+        $numargs = func_num_args();
+        if ($numargs === 1) {
+            $middleware = func_get_arg(0);
+            
+            array_push($this->middlewares, $middleware);
+           
+            return;
+        }
+        if ($numargs === 2) {
+            $prefixUrl = '/logistic/app/ws/express';
+            $url = $_SERVER['REQUEST_URI'];
+            $prefix = $prefixUrl . func_get_arg(0);
+           
+            
+            if (substr($url, 0, strlen($prefix)) !== $prefix) {
+                return;
+            }
+            $newUrl = substr($url, strlen($prefix));
+
+            $mw = func_get_arg(1);
+            $middleware = function($req, $res, $next) use ($mw, $newUrl) {
+                $req = new Request();
+                $req->url = $newUrl;
+                $mw($req, $res, $next);
+            };
+            array_push($this->middlewares, $middleware);
+            return;
+        }
+        
     }
 
     public function run() {
--- app/ws/express/server.php
+++ app/ws/express/server.php
@@ -4,17 +4,17 @@ require(__DIR__ . '/modules/express/index.php');
 require(__DIR__ . '/modules/log/index.php');
 
 $app = new Express();
-$app->use(function($req, $res, $next) {
+
+$app->use('/toto', function($req, $res, $next) {
     global $console;
     $console->log('$req->url', $req->url);
-    $res->send("Coucoux: $req->url");
-    $next();
+    $res->send("toto: $req->url");
 });
 
 $app->use(function($req, $res, $next) {
     global $console;
     $console->log('$req->url', $req->url);
-    $res->send("Coucou2: $req->url");
+    $res->send("Coucoux: $req->url");
 });
 
 
