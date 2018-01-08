54073cadfd5afbaa37a90db2ebd6c64fa5df7162
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-08 10:48:31 +0100

    add: load more btn on loader admin

--- app/admin/admin-loader/ctrl/admin-loader-ctrl.js
+++ app/admin/admin-loader/ctrl/admin-loader-ctrl.js
@@ -2,9 +2,22 @@ export function AdminLoadersCtrl($stateParams, adminLoader) {
 	'ngInject';
 	const ctrl = this;
 	ctrl.adminLoader = adminLoader;
+
+	ctrl.limit = function() {
+		this.limit = 40;
+		console.log('ctrl.limit', ctrl);
+	};
+
+	ctrl.loadMore = function() {
+		console.log('ctrl.loadMore', ctrl.loadMore);
+			const moreData = ctrl.limit + 10;
+			ctrl.limit = moreData > ctrl.loader.length ? ctrl.loader.length : moreData;
+	};
+
 	ctrl.$onInit = function() {
 		adminLoader.list().then(function(list) {
 			ctrl.loader = list;
+			ctrl.limit();
 			console.log('ctrl.loader', ctrl.loader);
 		}).catch(function(error) {
 			console.error('error', error);
