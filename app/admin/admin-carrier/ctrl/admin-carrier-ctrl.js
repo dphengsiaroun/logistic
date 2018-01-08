b4a368d8e1fb2486f9c59540f5811cfe985cc513
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-08 10:53:02 +0100

    add: load more btn

--- app/admin/admin-carrier/ctrl/admin-carrier-ctrl.js
+++ app/admin/admin-carrier/ctrl/admin-carrier-ctrl.js
@@ -2,10 +2,23 @@ export function AdminCarriersCtrl($stateParams, adminCarrier) {
 	'ngInject';
 	const ctrl = this;
 	ctrl.adminCarrier = adminCarrier;
+	
+	ctrl.limit = function() {
+		this.limit = 40;
+		console.log('ctrl.limit', ctrl);
+	};
+
+	ctrl.loadMore = function() {
+		console.log('ctrl.loadMore', ctrl.loadMore);
+			const moreData = ctrl.limit + 10;
+			ctrl.limit = moreData > ctrl.carrier.length ? ctrl.carrier.length : moreData;
+	};
+
 	console.log('AdminCarriersCtrl');
 	ctrl.$onInit = function() {
 		adminCarrier.list().then(function(list) {
 			ctrl.carrier = list;
+			ctrl.limit();
 			console.log('ctrl.carrier', ctrl.carrier);
 		}).catch(function(error) {
 			console.error('error', error);
--- app/admin/admin-proposal/ctrl/admin-proposal-ctrl.js
+++ app/admin/admin-proposal/ctrl/admin-proposal-ctrl.js
@@ -2,9 +2,22 @@ export function AdminProposalsCtrl($stateParams, adminProposal) {
 	'ngInject';
 	const ctrl = this;
 	ctrl.adminProposal = adminProposal;
+
+	ctrl.limit = function() {
+		this.limit = 40;
+		console.log('ctrl.limit', ctrl);
+	};
+
+	ctrl.loadMore = function() {
+		console.log('ctrl.loadMore', ctrl.loadMore);
+			const moreData = ctrl.limit + 10;
+			ctrl.limit = moreData > ctrl.proposal.length ? ctrl.proposal.length : moreData;
+	};
+
 	ctrl.$onInit = function() {
 		adminProposal.list().then(function(list) {
 			ctrl.proposal = list;
+			ctrl.limit();
 			console.log('ctrl.proposal', ctrl.proposal);
 		}).catch(function(error) {
 			console.error('error', error);
