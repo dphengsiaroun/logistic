34abd4d5d63f3d4ebcbc0c977d140bf04bdae864
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-01-31 11:10:12 +0100

    add: export csv user admin

--- app/admin/admin-user/ctrl/admin-user-ctrl.js
+++ app/admin/admin-user/ctrl/admin-user-ctrl.js
@@ -1,4 +1,4 @@
-export function AdminUsersCtrl($stateParams, adminUser) {
+export function AdminUsersCtrl($filter, $stateParams, adminUser, exportToCsv) {
 	'ngInject';
 	const ctrl = this;
 	ctrl.adminUser = adminUser;
@@ -16,11 +16,36 @@ export function AdminUsersCtrl($stateParams, adminUser) {
 
 	ctrl.$onInit = function() {
 		adminUser.list().then(function(list) {
-			ctrl.user = list;
+			ctrl.users = list;
 			ctrl.limit();
 			console.log('ctrl.user', ctrl.user);
 		}).catch(function(error) {
 			console.error('error', error);
 		});
+	};
+
+	ctrl.export2Csv = () => {
+		console.log('export2Csv');
+
+		let csv = ctrl.users.map((user) => {
+			return [
+				user.id,
+				user.content.lastname,
+				user.content.firstname,
+				user.login,
+				user.email,
+				user.phone,
+				user.content.address.street,
+				user.content.address.zipcode,
+				user.content.address.city,
+				user.content.address.country,
+				$filter('date')(new Date(user.content.created_t * 1000), `EEEE d LLLL yyyy à H'h'`)
+			];
+		}).join('\n');
+		csv = `Sep=,
+ID,NOM,PRENOM,LOGIN,EMAIL,TELEPHONE,ADRESSE,CODE POSTAL,VILLE,PAYS,DATE CREATION
+` + csv;
+
+		exportToCsv.export(csv);
 	};
 }
