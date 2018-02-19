c3f2c68d66570123de39681d460d7c10cfa938b5
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-02-19 11:36:09 +0100

    refactor: css

--- app/modules/business/lg-carrier/route/lg-carrier-create-truck-choose-route.component.js
+++ app/modules/business/lg-carrier/route/lg-carrier-create-truck-choose-route.component.js
@@ -3,25 +3,28 @@ import { breadcrumb } from '../carrier.breadcrumb.js';
 
 export const lgCarrierCreateTruckChooseRoute = {
 	template: carrierCreateTruckChooseHtml,
-	controller: function LgCarrierCreateTruckChooseRouteCtrl($state, connection, truck, carrier) {
+	controller: function LgCarrierCreateTruckChooseRouteCtrl($state, $stateParams, connection, truck, carrier) {
 		'ngInject';
 		this.breadcrumb = breadcrumb;
 		this.hasTruck = false;
 		this.truck = truck;
+
 		truck.empty().then(() => {
 			this.hasTruck = true;
+			console.log(this.hasTruck);
 		}).catch(() => {
 			this.hasTruck = false;
+			console.log(this.hasTruck);			
 		});
-		this.selectTruck = (t) => {
-			console.log('t', t);
-			console.log('carrier', carrier);
 
+		this.selectTruck = (t) => {
 			carrier.stepData.truck = t;
-			if (carrier.type === 'create') {
+			if (carrier.type === 'create' || carrier.type === undefined) {
+				console.log('carrier.type', carrier.type);				
 				$state.go('carrier:create');
 			} else {
-				$state.go('carrier:' + carrier.type, {login: connection.user.content.login, id: carrier.current.id});
+				console.log('carrier.type', carrier.type);
+				$state.go('carrier:' + carrier.type, {login: connection.user.content.login, id: carrier.stepData.truck.id});
 			}
 		};
 	}
