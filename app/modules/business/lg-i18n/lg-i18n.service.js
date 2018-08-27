8c9de78105228b454a260ff5a8f51db727011fe2
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-08-27 14:48:07 +0200

    id83: i18n

--- app/modules/business/lg-i18n/lg-i18n.service.js
+++ app/modules/business/lg-i18n/lg-i18n.service.js
@@ -1,35 +1,44 @@
+import './lg-i18n.scss';
+
 module.exports = 'lg-i18n';
 
 const FR = 'fr';
 // const AR = 'ar';
 
 class LgI18n {
-	constructor(connection, user) {
+	constructor($rootScope, $document, connection, user) {
 		this.current = FR;
 		this.connection = connection;
 		this.user = user;
+		this.$document = $document;
+		$rootScope.lgI18n = this;
 		this.sync();
 	}
 
 	sync() {
 		this.connection.waitForCheckConnection().then(() => {
 			this.current = this.connection.user.content.language;
-		}).catch(() => {});
+		}).catch(() => {}).then(() => {
+			const html = this.$document[0].querySelector('html');
+			console.log('html', html);
+			html.setAttribute('lang', this.current);
+		});
 	}
 
 	chooseLanguage(lang, user) {
 		console.log('chooseLanguage');
 		this.current = lang;
+
 		this.connection.waitForCheckConnection().then(() => {
 			console.log('hello');
 			this.connection.user.content.language = this.current;
 			this.user.patch();
-		}).catch(() => {});
+		}).catch(() => {}).then(() => {
+			this.sync();
+		});
 
 	}
 }
 
 angular.module(module.exports, [])
 	.service('lgI18n', LgI18n);
-
-
